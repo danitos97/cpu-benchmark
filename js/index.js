21cd7d1090f35fd8be5cbbb92d6e729dc425d142
Daniel Rivera <daniel_doni@hotmail.com>
2021-10-31 23:17:08 -0600

    scores

--- js/index.js
+++ js/index.js
@@ -8,49 +8,79 @@
 
     setSpeed(0);
 
-    // const d = id => {const e = document.querySelectorAll(id);return e.length==1?e[0]:e.length==0?null:[...e]}
+    // const d=id=>{const e=document.querySelectorAll(id);return e.length==1?e[0]:e.length==0?null:[...e]}
     // EventTarget.prototype.on = EventTarget.prototype.addEventListener
     // Array.prototype.on = function(ev,f){this.forEach(e=>{e.on(ev,f)})}
 
     const nCores = navigator.hardwareConcurrency || 32
+    const progress = document.querySelector('.progress-bar > div')
 
-    console.log(nCores)
-
-    let nThreads = 2
-    let isStarted = false
+ 
     let threads = []
 
     $('#start').on('click',function(){
-        isStarted = !isStarted
-        for(let i = 0; i < nThreads; i++)
-            isStarted? createThread() : removeThread()
-        $('#start b').html(isStarted? 'STOP' : 'START')
-        setSpeed(isStarted? calcSpeed() : 0)
-        $('#start').toggleClass('stop');
+        $(this).hide()
+        $('.progress-bar').addClass('visible')
+        $('.scores').show()
+        createThread()
+        setSpeed(calcSpeed())
+        updateBar(0)
     })
+
+    function updateBar(porcentage){
+        setTimeout(function(){
+            progress.innerHTML = `${porcentage}%`
+            if(porcentage == 30){
+                setSingleScore(1333)
+                for(let i = 1; i < nCores; i++)
+                    createThread()
+                setSpeed(100)
+            }
+            if(porcentage < 100)
+                updateBar(++porcentage)
+            else{
+                // $('#start').show()
+                $('.progress-bar').removeClass('visible')
+                for(let i = 0; i < nCores; i++)
+                    removeThread()
+                setSpeed(0)
+                setMultiScore(7686)
+            }
+        },10 * 10)
+        
+    }
+
+    function setSingleScore(score){
+        $("#single-core .loading").hide()
+        $('#single-core .score').html(score)
+    }
+
+    function setMultiScore(score){
+        $("#multi-core .loading").hide()
+        $('#multi-core .score').html(score)
+    }
     
-    $('#add').on('click', function() {
-        if(nThreads < nCores){
-            $('#n-threads').html(++nThreads)
-            if(isStarted) {
-                setSpeed(calcSpeed());
-                createThread()
-            }
-        }
-    })
-    $('#sub').on('click', function() {
-        if(nThreads > 1){
-            $('#n-threads').html(--nThreads)
-            if(isStarted) {
-                setSpeed(calcSpeed());
-                removeThread()
-            }
-        }
-    })
+    
+    // $('#add').on('click', function() {
+    //     if(nThreads < nCores){
+    //         $('#n-threads').html(++nThreads)
+    //         if(isStarted) {
+    //             setSpeed(calcSpeed());
+    //             createThread()
+    //         }
+    //     }
+    // })
+    // $('#sub').on('click', function() {
+    //     if(nThreads > 1){
+    //         $('#n-threads').html(--nThreads)
+    //         if(isStarted) {
+    //             setSpeed(calcSpeed());
+    //             removeThread()
+    //         }
+    //     }
+    // })
     function calcSpeed(){
-        let speed = Math.floor(nThreads * 100 / nCores + (nThreads / 3))
-        // if(speed < 100) speed += nThreads;
-        return speed
+        return Math.floor(100 / nCores + 8)
     }
     function createThread(){
         const hilo = new Worker('js/thread.js')
